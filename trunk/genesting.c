ec544afe718843b0aedd7f16b0645bea37a740d9
jcongote <jcongote@123446bb-9919-0410-8cb2-bf74317c8d72>
2006-09-27 15:49:43 +0000

    Ya estamos graficando, y se corrigieron algunos errores en las funciones.

--- trunk/genesting.c
+++ trunk/genesting.c
@@ -16,20 +16,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+#include <allegro.h>
+
+#define line ge_line
+#define polygon ge_polygon
 #include "polygon.h"
+#undef line
+#undef polygon
 
-struct _genesting{
-    polygon plantilla;
-    polygon *huecos;
-    polygon *patrones;
+
+struct _genesting
+{
+    ge_polygon plantilla;
+    ge_polygon *huecos;
+    ge_polygon *patrones;
     unsigned int nhuecos;
     unsigned int npatrones;
 };
 
 typedef struct _genesting genesting;
 
-genesting* leer_archivo(char *arc_name){
+genesting* leer_archivo(char *arc_name)
+{
     FILE* arc;
     genesting *g;
     int npoly;
@@ -44,39 +54,43 @@ genesting* leer_archivo(char *arc_name){
     g->nhuecos =0;
     g->npatrones =0;
 
-    g->huecos=(polygon*) malloc(sizeof(polygon)*npoly-1);
-    g->patrones=(polygon*) malloc(sizeof(polygon)*npoly-1);
+    g->huecos=(ge_polygon*) malloc(sizeof(ge_polygon)*npoly-1);
+    g->patrones=(ge_polygon*) malloc(sizeof(ge_polygon)*npoly-1);
 
-    for (i=0;i<npoly;i++){
-            int nvert,tipo;
-            polygon *p;
-            fscanf(arc,"%i %i",&nvert,&tipo);
-            switch(tipo){
-                    case 1:
-                    p=&(g->plantilla);
-                    break;
-                    case 2:
-                    p=&(g->patrones[g->npatrones++]);
-                    break;
-                    case 3:
-                    p=&(g->huecos[g->nhuecos++]);
-                    break;
-            }
-            p->nvertices = nvert;
-            p->v=(point*) malloc(sizeof(point)*nvert);
-            for (j=0;j<nvert;j++){
-                fscanf(arc,"%f %f",&(p->v[j].x),&(p->v[j].y));
-            }
+    for (i=0;i<npoly;i++)
+    {
+        int nvert,tipo;
+        ge_polygon *p;
+        fscanf(arc,"%i %i",&nvert,&tipo);
+        switch(tipo)
+        {
+            case 1:
+            p=&(g->plantilla);
+            break;
+            case 2:
+            p=&(g->patrones[g->npatrones++]);
+            break;
+            case 3:
+            p=&(g->huecos[g->nhuecos++]);
+            break;
+        }
+        p->nvertices = nvert;
+        p->v=(point*) malloc(sizeof(point)*nvert);
+        for (j=0;j<nvert;j++)
+        {
+            fscanf(arc,"%f %f",&(p->v[j].x),&(p->v[j].y));
+        }
     }
-    g->huecos=(polygon*) realloc(g->huecos,sizeof(polygon)*g->nhuecos);
-    g->patrones=(polygon*) realloc(g->patrones,sizeof(polygon)*g->npatrones);
+    g->huecos=(ge_polygon*) realloc(g->huecos,sizeof(ge_polygon)*g->nhuecos);
+    g->patrones=(ge_polygon*) realloc(g->patrones,sizeof(ge_polygon)*g->npatrones);
 
     fclose(arc);
 
     return g;
 };
 
-void show(genesting *g){
+void show(genesting *g)
+{
     printf("Mostrando informacion de genesting\n");
     printf("Plantilla:\n");
     printf("  Vertices: %i\n",g->plantilla.nvertices);
@@ -85,18 +99,132 @@ void show(genesting *g){
     printf("Patrones: %i\n",g->npatrones);
 }
 
-int main(int argc, char **argv){
+void draw_plantilla_huecos(BITMAP *bb,genesting *g){
+
+    int i,j;
+
+    int a,b,c,d;
+
+    acquire_screen();
+
+    //Dibujar Plantilla
+    for (i=0;i<g->plantilla.nvertices;i++)
+    {
+        a=(int)g->plantilla.v[i].x;
+        b=(int)g->plantilla.v[i].y;
+        c=(int)g->plantilla.v[(i+1)%g->plantilla.nvertices].x;
+        d=(int)g->plantilla.v[(i+1)%g->plantilla.nvertices].y;
+        line(bb,a,b,c,d,makecol(255,0,0));
+    }
+
+    //Dibujar Huecos
+    for (i=0;i<g->nhuecos;i++)
+    {
+        for (j=0;j<g->huecos[i].nvertices;j++)
+        {
+            a=(int)g->huecos[i].v[j].x;
+            b=(int)g->huecos[i].v[j].y;
+            c=(int)g->huecos[i].v[(j+1)%g->huecos[i].nvertices].x;
+            d=(int)g->huecos[i].v[(j+1)%g->huecos[i].nvertices].y;
+            line(bb,a,b,c,d,makecol(0,0,255));
+        }
+    }
+    release_screen();
+
+}
+
+void init()
+{
+    int depth, res;
+    allegro_init();
+    depth = desktop_color_depth();
+    if (depth == 0)
+        depth = 32;
+    set_color_depth(depth);
+    res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
+    if (res != 0)
+    {
+        allegro_message(allegro_error);
+        exit(-1);
+    }
+
+    install_timer();
+    install_keyboard();
+    install_mouse();
+    /* add other initializations here */
+}
+
+int main(int argc, char **argv)
+{
+    int i,j,k,l;
 
     genesting *g;
 
-    if (argc<2){
-            fprintf(stderr,"Se deben entrar ingresar el nombre del archivo a leer\n");
-            return 1;
+    if (argc<2)
+    {
+        fprintf(stderr,"Se deben entrar ingresar el nombre del archivo a leer\n");
+        return 1;
     }
 
     g=leer_archivo(argv[1]);
 
     show(g);
 
-	return 0;
+    init();
+    clear_to_color(screen,makecol(255,255,255));
+    draw_plantilla_huecos(screen,g);
+
+    for (i=0;i<640;i++)
+    {
+        acquire_screen();
+        for (j=0;j<480;j++)
+        {
+            point po;
+
+            po.x = i, po.y = j;
+
+            if (polygon_pointinPolygon(&g->plantilla, &po))
+            {
+                int inside_hueco=1;
+                for (k=0;k<g->nhuecos && inside_hueco==1;k++)
+                {
+                    if (polygon_pointinPolygon(&g->huecos[k], &po)){
+                        inside_hueco=0;}
+                }
+
+                if (inside_hueco)
+                {
+                    int min=(int)distance_pointPolygon(&po, &g->plantilla);
+
+                    for (k=0;k<g->nhuecos;k++)
+                    {
+                        int min2=(int)distance_pointPolygon(&po, &g->huecos[k]);
+                        if (min2<min){
+                                min=min2;
+                        }
+                    }
+                    _putpixel32(screen,i,j,makecol(min,min,min));
+                }
+            }
+        }
+        release_screen();
+    }
+
+    /*
+    Encontrando facilmente el medial axe, o la division de voronoi
+    si se tiene el area de cada uno de las celdas de voronoi y se
+    multiplica por la maxima altura
+    entonces se puede encontrar el volumen... dado que la figura
+    es un cono.
+
+    */
+
+    while (!key[KEY_ESC])
+    {
+        /* put your code here */
+    }
+
+    return 0;
 }
+END_OF_MAIN();
+
--- trunk/point.c
+++ trunk/point.c
@@ -4,17 +4,17 @@
 #include "point.h"
 
 
-float dot(point *a, point *b, point *c)
+float dot(point *a, point *b, point *c) //OK
 {
     return ((b->x - a->x) * (c->x - b->x)) + ((b->y - a->y) * (c->y - b->y));
 }
 
-float cross(point *a, point *b, point *c)
+float cross(point *a, point *b, point *c) //OK
 {
     return ((b->x - a->x)*(c->y - a->y)) - ((b->y - a->y)*(c->x - a->x));
 }
 
-float distance(point *a, point *b)
+float distance(point *a, point *b) //OK
 {
     return sqrt(powf(a->x - b->x,2.0)+powf(a->y - b->y,2.0));
 }
